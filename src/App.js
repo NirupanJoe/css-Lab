be317db392a1f2deec53b36fb97bac8d4acf2d17
nirupan <nirupanjoej@gmail.com>
2021-06-19 12:49:02 +0530

    introduce a container and changed px as percentage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="App-logo">
-			<div className="box b1"> </div>
-			<div className="box b2"> </div>
-			<div className="box b3"> </div>
-			<div className="box b4"> </div>
+		<div className="logo">
+			<div className="container">
+				<div className="box b1"> </div>
+				<div className="box b2"> </div>
+				<div className="box b3"> </div>
+				<div className="box b4"> </div>
+			</div>
 		</div>
 	</div>
 	;
